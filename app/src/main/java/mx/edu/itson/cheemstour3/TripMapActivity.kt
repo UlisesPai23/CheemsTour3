c5eec79108e2b7f193a16f7085c816f71eed9780
UlisesPai <guillermo.hernandez261429@potros.itson.edu.mx>
2026-04-23 12:31:44 -0700

    Listo para ejecutarse pero todavia no hace nada

--- app/src/main/java/mx/edu/itson/cheemstour3/TripMapActivity.kt
+++ app/src/main/java/mx/edu/itson/cheemstour3/TripMapActivity.kt
@@ -6,13 +6,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import mx.edu.itson.cheemstour3.entities.Trip
 import mx.edu.itson.cheemstour3.entities.utils.RetrofitUtil
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class TripMapActivity : AppCompatActivity() {
+class TripMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    var map : GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,7 +30,10 @@ class TripMapActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        getTrips()
+
+        var mapFragment = supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
 
     }
     fun getTrips(){
@@ -33,7 +44,11 @@ class TripMapActivity : AppCompatActivity() {
                 response: Response<List<Trip>?>
             ) {
                 val trips : List<Trip> = response.body()!!
-                for(t in trips){
+
+                trips.forEach { t ->
+                    val latLng = LatLng(t.latitude, t.longitude)
+
+                    map?.addMarker(MarkerOptions().position(latLng).title(t.name))
 
                 }
             }
@@ -46,5 +61,18 @@ class TripMapActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        try {
+
+            map = googleMap
+            map!!.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            getTrips()
+
+        } catch (ex: Exception){
+            Log.e("Error loading map", ex.message.toString())
+        }
     }
 }
